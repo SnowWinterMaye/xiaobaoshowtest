1b99c3372de5143be6cb375785f0217acf0c29ae
cty8998 <313477629@qq.com>
2016-04-28 11:09:55 +0800

    12

---
+++ directive.js
@@ -0,0 +1,15 @@
+ 'use strict';
+ angular.module("myApp",[])
+  .directive("triButton",function(){
+            return{
+                scope:{counter:"=counter"},
+                link:function(scope,element,attrs){
+                    element.on("click",function(event){
+                        console.log("Button click"+event.target.innerText);
+                        scope.$apply(function(){
+                            scope.counter++;
+                        });
+                    });
+                }
+            };
+        });
